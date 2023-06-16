9d24a1afd1b6b094211219347ef0c7f439479cad
HassanKachawa <99229716+HassanKachawa@users.noreply.github.com>
2023-06-16 07:54:12 +0530

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -1,20 +1,20 @@
 a=int(input("Enter 1st Number:"))
 b=int(input("Enter 2nd Number:"))
-Result=int(input("Enter Operator \n 1]Addition \n 2]Subtraction \n 3]Multiplication \n 4]Division \n 5]Floor Division \n 6]Modulus \n 7]Exponential \n:"))
+Result=(input("Enter operator:"))
 
-if Result==1:
+if Result==('+'):
     print("Addition:", a+b)
-if Result==2:
+if Result==('-'):
         print("Subtraction:", a-b)
-if Result==3:
+if Result==("*"):
         print("Multiplication:", a*b)
-if Result==4:
+if Result==("/"):
         print("Division:", a/b)
-if Result==4:
+if Result==("//"):
         print("Floor Division:", a//b)
-if Result==4:
+if Result==("%"):
         print("Modulus:", a%b)
-if Result==4:
+if Result==("**"):
         print("Exponential:", a**b)        
 
     
